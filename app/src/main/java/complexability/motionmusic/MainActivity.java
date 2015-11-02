3e333681e7f79e5812c5d7ac608c5a851514cbfa
dihmuzikien <turbo2547@hotmail.com>
2015-11-01 16:07:44 -0800

    Added button to launch activity

--- app/src/main/java/complexability/motionmusic/MainActivity.java
+++ app/src/main/java/complexability/motionmusic/MainActivity.java
@@ -1,7 +1,10 @@
 package complexability.motionmusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchGame(View view) {
+        Log.d("click", "launch Game");
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
 }
+
